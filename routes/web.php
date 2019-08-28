4848b1cce1c9052bbd75e85542b962fc2d00d61f
Sasha <sashok.brazhnik3@gmail.com>
2019-08-28 20:55:20 +0300

    day20

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+use App\Post;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+	$posts = Post::all();
+	
+    return view('index')->with('posts', $posts);
+});
+
+Route::get('/post', function () {
+	 return view ('post');
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
